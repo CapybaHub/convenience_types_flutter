01dfc26e92bb1aa66d5bebbf2f532753feb0cfbe
Kainã Terto <kterto@capyba.com>
2022-09-13 14:59:50 -0300

    barruel files

--- lib/convenience_types.dart
+++ lib/convenience_types.dart
@@ -1,13 +1,6 @@
 library convenience_types;
 
 // exporting types
-export 'package:convenience_types/types/result.dart';
-export 'package:convenience_types/types/request_status.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) {
-    return value + 1;
-  }
-}
+export 'package:convenience_types/types/types.dart';
+export 'package:convenience_types/errors/errors.dart';
+export 'package:convenience_types/util/utils.dart';
---
+++ lib/errors/errors.dart
@@ -0,0 +1,8 @@
+export 'package:convenience_types/errors/app_error.dart';
+export 'package:convenience_types/errors/cache_error.dart';
+export 'package:convenience_types/errors/default_error_messages.dart';
+export 'package:convenience_types/errors/device_info_error.dart';
+export 'package:convenience_types/errors/exceptions.dart';
+export 'package:convenience_types/errors/form_error.dart';
+export 'package:convenience_types/errors/http_error.dart';
+export 'package:convenience_types/errors/storage_error.dart';
